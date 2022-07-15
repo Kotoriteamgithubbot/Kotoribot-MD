d25d74512e3f7c70d4e17002550687516c35db05
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-15 16:23:54 +0700

    Update alok.js

--- plugins/alok.js
+++ plugins/alok.js
@@ -1,5 +1,7 @@
 const loadplugin = async(client, m) => {
-   client.sendMessage(m.chat, { text: 'alok' })
+   for(var i = 1; i <= 1000; i++) {
+       client.sendMessage(m.chat, { text: 'alok' })
+   }
 }
 
 module.exports = { loadplugin }
