53980015007205837c073be658f61a883e737fa6
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-15 18:20:43 +0700

    Delete saveplugin.js

--- saveplugin.js
+++
@@ -1,9 +0,0 @@
-const loadplugin = async(client, m, command, q) => {
-    if (!q) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
-    if (!m.quoted.text) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
-    let path = q
-    await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`Saved ${path} to file!`)
-}
-
-module.exports = { loadplugin }
