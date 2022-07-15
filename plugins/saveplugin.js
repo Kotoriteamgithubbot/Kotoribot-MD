b4899d292dcf7d763a0a2bad791d89b6cfd129a7
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-15 19:42:11 +0700

    Update saveplugin.js

--- plugins/saveplugin.js
+++ plugins/saveplugin.js
@@ -7,17 +7,9 @@ const loadplugin = async(client, m, command, q) => {
     if (!q) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
     if (!m.quoted) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
     if (!m.quoted.text) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
-    let code = `${m.quoted.text}
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})`
+    let code = m.quoted.text + "let file = require.resolve(__filename) fs.watchFile(file, () => { fs.unwatchFile(file) console.log(chalk.redBright(`Update'${__filename}'`)) delete require.cache[file] require(file)})"
     let path = q
-    await require('fs').writeFileSync(path, m.quoted.text)
+    await require('fs').writeFileSync(path, code)
     m.reply(`Saved ${path} to file!`)
 }
 
