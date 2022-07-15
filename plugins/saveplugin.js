23da0640436d208e63ab6341e8c9194714b967cc
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-15 18:21:13 +0700

    Create saveplugin.js

---
+++ plugins/saveplugin.js
@@ -0,0 +1,9 @@
+const loadplugin = async(client, m, command, q) => {
+    if (!q) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
+    if (!m.quoted.text) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
+    let path = q
+    await require('fs').writeFileSync(path, m.quoted.text)
+    m.reply(`Saved ${path} to file!`)
+}
+
+module.exports = { loadplugin }
