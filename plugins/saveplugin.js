2e4534388f9f51aea48933d0c309e4feaef48006
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-16 18:09:41 +0700

    Update saveplugin.js

--- plugins/saveplugin.js
+++ plugins/saveplugin.js
@@ -3,11 +3,11 @@
 Under license CloudbyPsn 
 */
 
-let handler = async(client, m, { command, q }) => {
-    if (!q) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
+let handler = async(client, m, { text }) => {
+    if (!text) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
     if (!m.quoted) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
     if (!m.quoted.text) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
-    let path = "./plugins/" + q
+    let path = "./plugins/" + text
     await require('fs').writeFileSync(path, m.quoted.text)
     m.reply(`Saved ${path} to file!`)
 }
