1c04c007a7877f0d78bcbddd6cdfd6e76d738535
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-16 18:18:48 +0700

    Update getplugin.js

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -3,7 +3,7 @@
 Under license CloudbyPsn 
 */
 
-let handler = async(client, m, { text }) => {
+let handler = async(client, m, text) => {
     if (!text) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
     let path = "./plugins/" + text
     let code = await require('fs').readFileSync(path, 'utf-8')
