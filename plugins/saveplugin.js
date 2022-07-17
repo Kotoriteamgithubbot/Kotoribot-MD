8141500a5ef7cff02293cf43778db214ac61d218
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-17 10:52:09 +0700

    Update saveplugin.js

--- plugins/saveplugin.js
+++ plugins/saveplugin.js
@@ -7,7 +7,7 @@ let handler = async(client, m, text) => {
     if (!text) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
     if (!m.quoted) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
     if (!m.quoted.text) return client.sendMessage(m.chat, { text: 'Reply code!' }, { quoted: m })
-    let path = "./plugins/" + text
+    let path = "./plugins/" + text + ".js"
     await require('fs').writeFileSync(path, m.quoted.text)
     m.reply(`Saved ${path} to file!`)
 }
