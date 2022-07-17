ad41bfffc6726fea924324768657ffa60806677b
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-17 10:51:42 +0700

    Update getplugin.js

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -5,7 +5,7 @@ Under license CloudbyPsn
 
 let handler = async(client, m, text) => {
     if (!text) return client.sendMessage(m.chat, { text: 'Masukkan path plugin!' }, { quoted: m })
-    let path = "./plugins/" + text
+    let path = "./plugins/" + text + ".js"
     let readdir = require('fs').readdirSync('./plugins')
     let arr = readdir.map(v => v.replace('.js', ''))
     if (!require('fs').existsSync(path)) return m.reply(`'${text}' tidak ditemukan!\n\n${arr.map(v => ' ' + v).join('\n')}`)
