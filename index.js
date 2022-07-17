8a9e2db4fef7a46db3bc093fb911e171fa2e0713
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-17 17:15:44 +0700

    update index.js

--- index.js
+++ index.js
@@ -1198,18 +1198,18 @@ case 'ytmp3': case 'ytaudio':
 let { yta } = require('./lib/y2mate')
 if (!q) client.sendMessage(m.chat, { text: mess.linkm }, { quoted: m })
 let qualityaudio = args[1] ? args[1] : '128kbps'
-let media = await yta(text, qualityaudio)
-if (media.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(media))
-client.sendImage(m.chat, media.thumb, `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '128kbps'}`, m)
-client.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
+let mediaaudio = await yta(text, qualityaudio)
+if (mediaaudio.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(mediaaudio))
+client.sendImage(m.chat, mediaaudio.thumb, `⭔ Title : ${mediaaudio.title}\n⭔ File Size : ${mediaaudio.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '128kbps'}`, m)
+client.sendMessage(m.chat, { audio: { url: mediaaudio.dl_link }, mimetype: 'audio/mpeg', fileName: `${mediaaudio.title}.mp3` }, { quoted: m })
 break
 case 'ytmp4': case 'ytvideo': 
 let { ytv } = require('./lib/y2mate')
 if (!q) client.sendMessage(m.chat, { text: mess.linkm }, { quoted: m })
 let qualityvideo = args[1] ? args[1] : '360p'
-let media = await ytv(text, qualityvideo)
-if (media.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(media))
-client.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
+let mediavideo = await ytv(text, qualityvideo)
+if (mediavideo.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(mediavideo))
+client.sendMessage(m.chat, { video: { url: mediavideo.dl_link }, mimetype: 'video/mp4', fileName: `${mediavideo.title}.mp4`, caption: `⭔ Title : ${mediavideo.title}\n⭔ File Size : ${mediavideo.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
 break
 case 'suitpvp': case 'suit': 
 if (isBan) return m.reply(mess.ban)
