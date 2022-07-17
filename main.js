3e1acc2d7d3cb40938a9ceb36c893e7741d7fcc1
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-17 13:08:17 +0700

    update main.js

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ async function start() {
     
     //Grup Update
     client.ev.on('groups.update', async pea => {
-         client.sendMessage(owner+'@s.whatsapp.net', {text:JSON.stringify(pea)})
+         client.sendMessage(owner[0]+'@s.whatsapp.net', {text:JSON.stringify(pea)})
         //Get Profile Picture Group
         try {
            ppgc = await client.profilePictureUrl(pea[0].id, 'image')
