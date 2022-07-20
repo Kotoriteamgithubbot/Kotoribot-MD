f8ac2461b5e38b1698191b27176301359bd40752
Adesty_prsk <94209612+Adesti0403@users.noreply.github.com>
2022-07-20 21:24:59 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,7 +2,7 @@ const hx = require('hxz-api')
 
 let handler = async (client, m, text) => {
   if (!(text)) throw `Masukkan link Instagram yang ingin didownload!`
-  hx.igdl(args).then(async (r) => {
+  hx.igdl(text).then(async (r) => {
   for (let i = 0; i < r.medias.length; i++) {
     client.sendFile(m.chat, r.medias[i].url, '', `*${wm}*`, m)
     }
