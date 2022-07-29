36120bc7e38017a5f9fe95bc8e3ab4dcdf3a3b81
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-07-29 08:50:17 +0700

    update debounce.js

--- plugins/debounce.js
+++ plugins/debounce.js
@@ -7,11 +7,13 @@ Under license CloudbyPsn
 const { spawn }  = require('child_process');
 
 let handler = async(client, m) => {
+    if (!process.send) m.reply('Dont: node main.js\nDo: node start.js')
     await m.reply('Sedang Mereset Bot...\nMohon tunggu sekitar 1 menit')
     process.send('reset')
+    m.reply('Selesai Mereset Bot...')
 }
 
 handler.command =  /debounce/i
 handler.owner = true
 
-module.exports = { handler }
+module.exports = handler
