54111ae27ea150ca8702ecf6363c9dbb805ff493
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-08-07 21:18:50 +0700

    update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ global.antitags = true
 global.mess = {
 	logout: 'Kamu harus login terlebih dahulu! Ketik .login',
 	login: 'Kamu sudah login sebelumnya!',
-	register: 'Username atau email sudah pernah terdaftar!',
+	register: 'Username sudah pernah terdaftar!',
     success: 'Success',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
