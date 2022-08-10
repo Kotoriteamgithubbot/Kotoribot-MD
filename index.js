f18c0c7caa576f5d836289274ee546c422466bd3
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-08-10 16:38:58 +0700

    Maaf typo

--- index.js
+++ index.js
@@ -1376,14 +1376,14 @@ break
 case 'public': 
 if (!isLogin) return m.reply(mess.logout)
 if (!isCreator) return m.reply(mess.owner)
-global.db.data.bot.use = true
+global.db.data.bot.use = "public"
 m.reply('Sukse Change To Public Usage')
 addTypeCmd(command, 1)
 break
 case 'self': 
 if (!isLogin) return m.reply(mess.logout)
 if (!isCreator) return m.reply(mess.owner)
-global.db.data.bot.use = false
+global.db.data.bot.use = "self"
 m.reply('Sukses Change To Self Usage') 
 addTypeCmd(command, 1)          
 break
