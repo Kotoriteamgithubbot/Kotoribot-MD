a888fb0b6e35173edc95d68fe26102c62fdda2e2
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2022-08-16 09:38:36 +0700

    update index.js

--- index.js
+++ index.js
@@ -1855,15 +1855,6 @@ const sessionget = await fs.readFileSync('./session.json')
 await client.sendMessage(m.chat, { document: sessionget, mimetype: 'application/json', fileName: 'session.json' }, { quoted: m })
 addTypeCmd(command, 1)
 break
-case 'addcase':
-const codeIndexBefore = await fs.readFileSync('./index.js', 'utf-8')
-const codeIndexAfter = codeIndexBefore.replace(`let addCaseDefaultVariabelByPSN;`, `${m.quoted.text}`)
-await fs.writeFileSync('index.js', codeIndexAfter)
-m.reply(mess.success)
-break
-
-let addCaseDefaultVariabelByPSN;
-
 // Default
 default:
        if (isCmd && prefix != '' && !handlerPlugin) {
