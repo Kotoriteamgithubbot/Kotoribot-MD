925c60444c818ddb253118984961d9731958d6d2
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-08-23 17:20:03 +0700

    biar tau klo pluginnya ngerespon

--- plugins/speedtest.js
+++ plugins/speedtest.js
@@ -2,15 +2,16 @@ let cp = require('child_process')
 let { promisify } = require('util')
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (client, m) => {
-    let o
+    m.reply('Testing Speed...');
+    let o;
     try {
-        o = await exec('python speed.py')
+        o = await exec('python speed.py');
     } catch (e) {
-        o = e
+        o = e;
     } finally {
-        let { stdout, stderr } = o
-        if (stdout.trim()) m.reply(stdout)
-        if (stderr.trim()) m.reply(stderr)
+        let { stdout, stderr } = o;
+        if (stdout.trim()) m.reply(stdout);
+        if (stderr.trim()) m.reply(stderr);
     }
 }
 handler.login = true
