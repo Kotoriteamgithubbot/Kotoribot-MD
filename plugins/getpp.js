6256b088dd5a0d04f130b0f0590ba8b810aaa0da
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-08-29 06:52:45 +0700

    update getpp.js

--- plugins/getpp.js
+++ plugins/getpp.js
@@ -1,1 +1,14 @@
+let handler = async(client, m, text, args) => {
+    const jid  = m.mentionedJid[0] ? m.mentionedJid[0] : args[0] ? `${args[0]}@s.whatsapp.net` : m.sender
+    try {
+       resultUrl = await client.profilePictureUrl(jid, 'image')
+    } catch {
+       resultUrl = 'https://tinyurl.com/yx93l6da'
+    }
+    await client.sendMessage(m.chat, { image: { url: resultUrl }, mentions: [jid], caption: `@${jid.split("@")[0]}` })
+}
 
+handler.login = true
+handler.command = /^(get(pp|profile))$/i
+
+module.exports = handler
