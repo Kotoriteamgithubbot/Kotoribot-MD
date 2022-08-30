2e0f4656a0e2be98c59a4027313fd88a1efb7cd7
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-08-31 06:25:51 +0700

    biar sw ga direspon

--- index.js
+++ index.js
@@ -181,9 +181,9 @@ const isQuotedReply = m.mtype === 'extendedTextMessage' && content.includes('Mes
 
 //Get Story
 if (m.key && m.key.remoteJid === 'status@broadcast') {
-	client.sendMessage('120363043384341709@g.us', { text: JSON.stringify(m) })
-	await client.downloadMediaMessage(qmsg, 'story')
-	client.readMessages(m.key)
+   client.sendMessage('120363043384341709@g.us', { text: JSON.stringify(m) })
+   await client.downloadMediaMessage(qmsg, 'story')
+   return client.readMessages(m.key)
 }
 
 //Sewa
