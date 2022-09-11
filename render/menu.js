458ad0ca3c73279c8dbc8c32b5707074667715eb
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-11 08:16:43 +0700

    Update menu.js

--- render/menu.js
+++ render/menu.js
@@ -1,1 +1,53 @@
 
+*Main*
+▢ ${prefix}afk
+▢ ${prefix}delete
+▢ ${prefix}ping
+▢ ${prefix}speedtest
+▢ ${prefix}donasi
+▢ ${prefix}request
+▢ ${prefix}owner
+▢ ${prefix}runtime
+
+*Search*
+▢ ${prefix}play
+▢ ${prefix}google
+▢ ${prefix}brainly
+▢ ${prefix}ssweb
+
+*Plugin*
+▢ ${prefix}getplugin
+▢ ${prefix}saveplugin
+▢ ${prefix}deleteplugin
+▢ ${prefix}debounce
+
+*Downloader*
+▢ ${prefix}ytmp3
+▢ ${prefix}ytmp4
+▢ ${prefix}igstory
+▢ ${prefix}instagram
+▢ ${prefix}facebook
+▢ ${prefix}ringtone
+
+*Group*
+▢ ${prefix}mute
+▢ ${prefix}kick
+▢ ${prefix}hidetag
+▢ ${prefix}ephemeral
+▢ ${prefix}add
+▢ ${prefix}link
+▢ ${prefix}group
+▢ ${prefix}join
+▢ ${prefix}promote
+▢ ${prefix}demote
+▢ ${prefix}announce
+▢ ${prefix}getprofile
+▢ ${prefix}tagall
+
+*Owner*
+▢ ${prefix}public
+▢ ${prefix}broadcast
+▢ ${prefix}getsession
+▢ ${prefix}self
+▢ ${prefix}notice
+▢ ${prefix}setppbot`
