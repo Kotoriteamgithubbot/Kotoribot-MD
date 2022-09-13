9c5acf72c91fb712b8b3e7594150858c3556c8a9
Adesty_prsk <94209612+Adesti0403@users.noreply.github.com>
2022-09-13 18:32:43 +0800

    update clearchat.js

---
+++ plugins/clearchat.js
@@ -0,0 +1,14 @@
+/**
+© Create by Amirul Dev
+Under license CloudbyPsn 
+*/
+
+let handler = async(m, { client }) => {
+	m.reply(mess.later)
+    client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, m.chat)
+    m.reply(`${sign} Chat ini telah dihapus di bot.`)
+}
+
+handler.command = /^(clear|clearchat)$/i
+
+module.exports = handler 
