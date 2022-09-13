37dedb92b94267f3686d17a5805741867c958276
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-13 17:42:46 +0700

    update clearchat.js

--- plugins/clearchat.js
+++ plugins/clearchat.js
@@ -6,7 +6,7 @@ Under license CloudbyPsn
 let handler = async(m, { client }) => {
 	m.reply(mess.later)
     client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, m.chat)
-    m.reply(`${sign} Chat ini telah dihapus di bot.`)
+    m.reply(`${sign} Chat ini telah dihapus di bot`)
 }
 
 handler.command = /^(clear|clearchat)$/i
