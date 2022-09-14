0981d46be43549245b44fc1749ae8583965670ac
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-14 17:59:30 +0700

    update clearchatall.js

--- plugins/clearchatall.js
+++ plugins/clearchatall.js
@@ -9,7 +9,7 @@ let handler = async(m, { client, store }) => {
         const chats = store.chats.all().map(v => v.id).filter(v => v != 'status@broadcast')
         for (let id of chats) {
            client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }]}, id)
-           await client.delay(1000)
+           await client.delay(1500)
            client.sendMessage(id, { text: `${sign} Berhasil membersihkan semua chat` })
         }
      } catch {
