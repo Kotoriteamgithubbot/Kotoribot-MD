b88e92b7c20c9874db6839ecc4ae176c0e192bb2
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-14 15:35:38 +0700

    update clearchatall.js

---
+++ plugins/clearchatall.js
@@ -0,0 +1,22 @@
+/**
+© Create by Natia Shalsabilla
+Under license CloudbyPsn 
+*/
+
+let handler = async(m, { client, store }) => {
+	m.reply(mess.later)
+	try {
+        const chats = store.chats.all().map(v => v.id).filter(v => v != 'status@broadcast')
+        for (let id of chats) {
+           client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }]}, id)
+           m.reply(`${sign} Berhasil membersihkan semua chat`)
+        }
+     } catch {
+        m.reply(`${sign} Gagal membersihkan semua chat`)
+     }
+}
+
+handler.command = /^(clearall|clearchatall)$/i
+handler.owner = true
+
+module.exports = handler 
