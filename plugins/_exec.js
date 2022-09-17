a9145d3d904b99445302f5014d7d2d6dedf5a28c
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-17 10:12:08 +0700

    Update _exec.js

--- plugins/_exec.js
+++ plugins/_exec.js
@@ -1,1 +1,22 @@
+let cp = require('child_process')
+let { promisify } = require('util')
+let exec = promisify(cp.exec).bind(cp)
+let handler = async (m, { client, text }) => {
+  m.reply('Executing...')
+  let o
+  try {
+    o = await exec(text.trim())
+  } catch (e) {
+    o = e
+  } finally {
+    let { stdout, stderr } = o
+    if (stdout.trim()) m.reply(stdout)
+    if (stderr.trim()) m.reply(stderr)
+  }
+}
+
+handler.command = /^exec$/i
+handler.owner = true
+
+module.exports = handler
 
