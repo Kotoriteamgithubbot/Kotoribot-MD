b2124d2282ab6911760613374b0406e82ea0b4ef
Natia Shalsabilla <90103953+stafbotz@users.noreply.github.com>
2022-09-22 17:37:37 +0700

    Update clearchatall.js

--- plugins/clearchatall.js
+++ plugins/clearchatall.js
@@ -4,17 +4,17 @@ Under license CloudbyPsn
 */
 
 let handler = async(m, { client, store }) => {
-	m.reply(mess.later)
-	try {
-        let chats = store.chats.all().map(v => v.id).filter(v => v != 'status@broadcast')
-        for (let id of chats) {
-           client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }]}, id)
-           await client.delay(1500)
-           client.sendMessage(id, { text: `${sign} Berhasil membersihkan semua chat` })
-        }
-     } catch {
-        m.reply(`${sign} Gagal membersihkan semua chat`)
-     }
+    m.reply(mess.later)
+    try {
+      let chats = store.chats.all().map(v => v.id).filter(v => v != 'status@broadcast')
+      for (let id of chats) {
+         client.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }]}, id)
+         await client.delay(1500)
+         client.sendMessage(id, { text: `${sign} Berhasil membersihkan semua chat` })
+      }
+    } catch {
+       m.reply(`${sign} Gagal membersihkan semua chat`)
+    }
 }
 
 handler.command = /^(clearall|clearchatall)$/i
